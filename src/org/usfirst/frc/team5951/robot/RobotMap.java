c89f2023f3176c39d30aac51ac443604c3260552
Matan <mmann258@gmail.com>
2018-01-27 18:44:18 +0200

    Shooter subsystem

--- src/org/usfirst/frc/team5951/robot/RobotMap.java
+++ src/org/usfirst/frc/team5951/robot/RobotMap.java
@@ -14,13 +14,10 @@ package org.usfirst.frc.team5951.robot;
  * floating around.
  */
 public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+	//shooter
+	public static final int k_SHOTER_HIGH_A_TALON=1;
+	public static final int k_SHOOTER_HIGH_B_TALON=2;
+	public static final int k_SHOOTER_LOW_A_TALON=3;
+	public static final int k_SHOOTER_LOW_B_TALON=4;
+	public static final int k_SHOOTER_HIGH_A_TALON=1;
 }
---
+++ src/org/usfirst/frc/team5951/robot/commands/StopShot.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team5951.robot.commands;
+
+import org.usfirst.frc.team5951.robot.Robot;
+import org.usfirst.frc.team5951.robot.subsystems.Shooter;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class StopShot extends Command {
+	private Shooter shooter;
+
+	public StopShot() {
+		shooter = Robot.SHOOTER;
+		requires(shooter);
+	}
+
+	protected void initialize() {
+	}
+
+	protected void execute() {
+		shooter.stop();
+	}
+
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
