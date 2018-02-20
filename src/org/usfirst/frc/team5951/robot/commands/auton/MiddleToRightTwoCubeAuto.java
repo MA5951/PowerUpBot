3e5a8ed00f25b62cb9e4814e24e1fe9efc1ea847
Adi Oded <adioded2002@gmail.com>
2018-02-20 23:37:45 +0200

    Worked on 2 cube auto

--- src/org/usfirst/frc/team5951/robot/commands/auton/MiddleToRightTwoCubeAuto.java
+++ src/org/usfirst/frc/team5951/robot/commands/auton/MiddleToRightTwoCubeAuto.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team5951.robot.commands.auton;
 
+import org.usfirst.frc.team5951.robot.commands.caliber.ReleaseCube;
 import org.usfirst.frc.team5951.robot.commands.caliber.RetractCube;
 import org.usfirst.frc.team5951.robot.commands.caliber.SquishCube;
 import org.usfirst.frc.team5951.robot.commands.caliber.SwitchPosition;
@@ -25,9 +26,9 @@ public class MiddleToRightTwoCubeAuto extends CommandGroup {
 		// Shoot cube
 		addSequential(new SquishCube());
 		addSequential(new SwitchPosition());
-		addSequential(new TurnToAngleOneSide(35));
+		addSequential(new TurnToAngleOneSide(32));
 		
-		addSequential(new DriveStraight(1.7));
+		addSequential(new DriveStraight(1.8));
 		addSequential(new TurnToAngleOneSide(-21), 0.5);
 		addSequential(new CaliberShootOnly());
 		// Take anoter cube
@@ -37,20 +38,24 @@ public class MiddleToRightTwoCubeAuto extends CommandGroup {
 		addSequential(new TurnToAngle(-45.0));
 		// Intake the cube
 		addParallel(new DriveStraight(1.1));
-		addSequential(new IntakeCube(), 2.0); // TODO test
+		addSequential(new IntakeCube(), 2.0); 
+		addSequential(new WaitCommand(0.4));
+		addSequential(new ReleaseCube(), 1.0); 
+		addSequential(new WaitCommand(0.4));
+		addParallel(new IntakeCube(), 2.0); 
 		addSequential(new RetractCube());
 		addSequential(new WaitCommand(0.4));
 		addSequential(new IntakeCube(), 2.0);
 		addSequential(new WaitCommand(0.4));
-		// Shoot another cube
 		addSequential(new OpenIntakeGroup());
 		addSequential(new SwitchPosition(), 10);
 		addSequential(new CloseIntakeGroup());
+		// Shoot another cube
 		addSequential(new SquishCube());
 		addSequential(new DriveStraight(-0.8));
 		addSequential(new TurnToAngle(45.0));
-		addParallel(new DriveStraight(0.8), 3);
-		addSequential(new WaitCommand(1.2));
+		addSequential(new DriveStraight(1.1),4);
+		addSequential(new WaitCommand(0.2));
 		addSequential(new CaliberShootOnly());
 
 	}
